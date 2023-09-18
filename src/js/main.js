f4b7a68c4ce295611084484a130493c6cd8f6779
kamran-shv <eprixgroup@gmail.com>
2023-09-18 12:17:56 +0400

    ver 3.7

--- src/js/main.js
+++ src/js/main.js
@@ -60,7 +60,7 @@ sendBtn.addEventListener('click', () => {
     let subject = `${currentOffer}`.toString();
     Email.send({
         SecureToken: '09a85a1a-a9d1-4b3c-8298-2d0fc230cabf',
-        To: 'cavidhaciyev00@mail.ru',
+        To: 'cavidhaciyev00@mail.ru, kamranelli@gmail.com',
         From: "kamranelli@gmail.com",
         Subject: subject,
         Body: body
